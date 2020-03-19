dfd9e3d6e2479c7cf9b80232a1c3533d3c71ddbd
Crowdbotics <team@crowdbotics.com>
2020-03-19 09:40:18 +0000

    Initial commit.

---
+++ johnson_dev_2062/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for johnson_dev_2062 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'johnson_dev_2062.settings')
+
+application = get_wsgi_application()
